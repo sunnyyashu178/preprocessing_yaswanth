b29540e4a697fbd955f2d2203039535dd4329c4e
Yaswanth Babu Rayarapu <sunnyyashu178@gmail.com>
2020-12-19 23:36:47 +0100

    Python package for https://bit.ly/intro_nlp

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("READ.md","r") as file:
+	long_description = file.read()
+
+setuptools.setup(
+	name = "preprocessing_yaswanth", #this should be unique
+	version = "0.0.1",
+	author = "Yaswanth Babu",
+	author_email = "sunnyyashu178@gmail.com",
+	description = "This is preprocessing package",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	package = setuptools.find_packages(),
+	classifiers = ["Programming Language :: Python :: 3",
+	"License :: OSI Aproved :: MIT License",
+	"Operating System :: OS Independent"],
+	python_requires = ">=3.5"
+	)
